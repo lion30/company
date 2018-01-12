ed90d657711478699fdd6004a2d8357c8870f05c
lion30 <liwang_30@126.com>
2018-01-12 17:18:33 +0800

    20180112 commit company/urls.py

--- company/urls.py
+++ company/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import url, include
 from django.contrib import admin
 from .views import index
@@ -21,9 +22,17 @@ from .views import index
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index),
-    url(r'^sales/', include('sales_department.urls')),
-    url(r'^human_res/', include('human_res_department.urls')),
-    url(r'^pre_sale/', include('pre_sales_department.urls')),
     url(r'^design/', include('design_department.urls')),
-
+    url(r'^develop/', include('develop_department.urls')),
+    url(r'^engineer/', include('develop_department.urls')),
+    url(r'^human_res/', include('human_res_department.urls')),
+    url(r'^integrated/', include('integrated_department.urls')),
+    url(r'^marketing/', include('marketing_department.urls')),
+    url(r'^pre_sale/', include('pre_sales_department.urls')),
+    url(r'^pruduct/', include('pruduct_department.urls')),
+    url(r'^purchase/', include('purchase_department.urls')),
+    url(r'^quality/', include('quality_department.urls')),
+    url(r'^sales/', include('sales_department.urls')),
+    url(r'^tech_support/', include('tech_support_department.urls')),
+    url(r'^test/', include('test_department.urls')),
 ]
