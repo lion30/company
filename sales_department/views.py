edb90241ce97f5fd059e9fe0a99bb60c1585017c
lion30 <liwang_30@126.com>
2018-06-02 11:04:24 +0800

    9th commit begin to write homepage views

--- sales_department/views.py
+++ sales_department/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import View, DetailView
+from .models import Contractclue
 
 
-def sales_index(request):
+class Sales_index(View):
+    def init_data(self):
+        contract_clue = Contractclue.objects.all()
+    def get(self):
+        self.init_data(contract_clue)
     template_name = 'sales.html'
+
+
     return render(request, template_name)
